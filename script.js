5288fd9bed356b1518cc63c386cdc2277b15f1a7
MOHAMED FAIZAL M.S <108245146+faizalmsdev@users.noreply.github.com>
2023-06-30 22:28:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function updateTextWithPromise() {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          const message = 'Hello, world!';
+          resolve(message);
+        }, 1000);
+      });
+    }
+
+    updateTextWithPromise()
+      .then((message) => {
+        const outputElement = document.getElementById('output');
+        outputElement.innerText = message;
+      })
+      .catch((error) => {
+        console.error('An error occurred:', error);
+      });
